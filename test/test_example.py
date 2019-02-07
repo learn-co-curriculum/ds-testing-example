9a5676a496723676bd5130ee4b89b2174e318f7c
Mike Kane <guido.vanrawesome@gmail.com>
2019-02-07 13:17:38 -0800

    update testing example

--- test/test_example.py
+++ test/test_example.py
@@ -5,7 +5,16 @@
 # os.chdir(path[0])
 # print(os.getcwd())
 
-from ipynb.fs.full.index import two_plus_two
+from ipynb.fs.full.index import q1, q2, q3, q4
 
-def test_two_plus_two():
-    assert two_plus_two == 4
+def test_q1():
+    assert q1 == 4
+
+def test_q2():
+    assert q2 == 10
+
+def test_q3():
+    assert q3 <= 16
+
+def test_q4():
+    assert q4 == True
